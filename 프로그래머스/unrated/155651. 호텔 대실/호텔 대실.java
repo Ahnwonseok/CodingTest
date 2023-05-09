5658b31e1eee665e5911870aa46a8d64c7035a42
Ahnwonseok <95980876+Ahnwonseok@users.noreply.github.com>
2023-05-09 21:43:05 +0900

    [unrated] Title: 호텔 대실, Time: 36.95 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] book_time) {
+        int answer = 0;
+        int[][] changeTimes;
+        changeTimes = convertTime(book_time);
+        changeTimes = orderBytime(changeTimes);
+
+        List<Integer> endTimeList = new ArrayList<>();
+        for(int[] time : changeTimes){
+            boolean flag = false;
+            for(int i=0; i<endTimeList.size(); i++){
+                if(time[0]>=endTimeList.get(i)) {
+                    endTimeList.set(i,time[1]);
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag==false) {
+                endTimeList.add(time[1]);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static int[][] convertTime(String[][] times){
+        int[][] changeTimes = new int[times.length][2];
+
+        for (int i=0; i<times.length; i++){
+            int changeTime1 = Integer.parseInt(times[i][0].split(":")[0])*60 + Integer.parseInt(times[i][0].split(":")[1]);
+            int changeTime2 = Integer.parseInt(times[i][1].split(":")[0])*60 + Integer.parseInt(times[i][1].split(":")[1]) + 10;
+            changeTimes[i][0] = changeTime1;
+            changeTimes[i][1] = changeTime2;
+        }
+
+        return changeTimes;
+    }
+
+    public static int[][] orderBytime(int[][] times){
+        int tmpIndex=0;
+        for (int i = 0; i<times.length-1; i++){
+            tmpIndex = i;
+            for(int j = i+1; j< times.length; j++){
+                if(times[tmpIndex][0]>times[j][0]) {
+                    tmpIndex = j;
+                }
+                else if(times[tmpIndex][0]==times[j][0]){
+                    if(times[tmpIndex][1]>times[j][1])
+                        tmpIndex = j;
+                }
+            } 
+            int[] tmp = times[i];
+            times[i] = times[tmpIndex];
+            times[tmpIndex] = tmp;
+        }
+        return times;
+    }
+}
