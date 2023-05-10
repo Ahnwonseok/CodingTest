6225e36d360e7ec842299b1642b63498bc9f913b
Ahnwonseok <95980876+Ahnwonseok@users.noreply.github.com>
2023-05-10 23:34:39 +0900

    Update 요격 시스템.java

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -1,25 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] targets) {
-        int answer = 0;
-        
+    public static int solution(int[][] targets) {
+        //처음 요격 미사일을 넣고 시작
+        int answer = 1;
+        //폭격 미사일의 끝 좌표를 기준으로 정렬
         Arrays.sort(targets, (o1, o2) -> { return o1[1]-o2[1]; });
-        
-        int last = -1;
-        for (int[] target : targets) {
-            if (last == -1) {
+
+        int last = targets[0][1] //첫 폭격 미사일의 끝 좌표 == 요격 미사일이 발사되는 x좌표
+
+        for(int[] target : targets){
+            if (last <= target[0]) {
                 answer++;
-                last = target[1] - 1;
-                continue;
+                last = target[1];
             }
-            
-            if (last >= target[0] && last <= target[1]) continue;
-            
-            answer++;
-            last = target[1] - 1;
         }
-        
         return answer;
     }
 }
