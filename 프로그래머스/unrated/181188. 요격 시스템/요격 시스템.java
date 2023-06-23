7a919d482583c55d752eef70a26172627c4c8465
안원석 <95980876+Ahnwonseok@users.noreply.github.com>
2023-06-23 10:37:32 +0900

    [unrated] Title: 요격 시스템, Time: 0.49 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -2,13 +2,11 @@ import java.util.*;
 
 class Solution {
     public static int solution(int[][] targets) {
-        //처음 요격 미사일을 넣고 시작
-        int answer = 1;
-        //폭격 미사일의 끝 좌표를 기준으로 정렬
+        int answer = 0;
         Arrays.sort(targets, (o1, o2) -> { return o1[1]-o2[1]; });
 
-        int last = targets[0][1] //첫 폭격 미사일의 끝 좌표 == 요격 미사일이 발사되는 x좌표
-
+        int last = 0;
+        
         for(int[] target : targets){
             if (last <= target[0]) {
                 answer++;
