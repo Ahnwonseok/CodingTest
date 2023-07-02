11462fd73adc5aa2ae7ae1baf84f871c78983632
안원석 <95980876+Ahnwonseok@users.noreply.github.com>
2023-07-03 08:08:35 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 21.77 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,45 @@
+class Solution {
+    public long solution(long r1, long r2) {
+        long answer = 0;
+        long y1 = 0;
+        long y2 = 0;
+        long line = 0;
+
+        Solution twoCircle = new Solution();
+        y1 = twoCircle.innerNum(false, r1);
+        y2 = twoCircle.innerNum(false, r2);
+        line = twoCircle.innerNum(true, r1);
+
+        // System.out.println(y1);
+        // System.out.println(y2);
+        // System.out.println(line);
+
+        answer = y2 - y1 + line;
+        //System.out.println(answer);
+        return answer;
+    }
+
+    public long innerNum(boolean flag, long r){
+        long x = 1;
+        long y = 0;
+        long sum_y = 0;
+        long line = 0;
+
+        while (x <= r) {
+            if(Math.sqrt(Math.pow(x,2)+Math.pow(r-y,2)) < r){
+                sum_y += r - y + 1;
+                x++;
+            }
+            else if(Math.sqrt(Math.pow(x,2)+Math.pow(r-y,2)) == r){
+                sum_y += r - y + 1;
+                line++;
+                //System.out.println("line: "+x);
+                x++;
+            }
+            else y++;
+        }
+
+        if (flag) return line * 4;
+        else return sum_y * 4;
+    }
+}
