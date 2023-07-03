a200a7b0a44bf8823e2cd572377252fb792eeebe
안원석 <95980876+Ahnwonseok@users.noreply.github.com>
2023-07-03 10:06:46 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 17.65 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,45 +1,22 @@
 class Solution {
     public long solution(int r1, int r2) {
-        long answer = 0;
-        long y1 = 0;
-        long y2 = 0;
-        long line = 0;
-
-        Solution twoCircle = new Solution();
-        y1 = twoCircle.innerNum(false, r1);
-        y2 = twoCircle.innerNum(false, r2);
-        line = twoCircle.innerNum(true, r1);
-
-//        System.out.println(y1);
-//        System.out.println(y2);
-//        System.out.println(line);
-
-        answer = y2 - y1 + line;
-        //System.out.println(answer);
-        return answer;
-    }
-
-    public long innerNum(boolean flag, long r){
-        long x = 1;
-        long y = 0;
-        long sum_y = 0;
-        long line = 0;
-
-        while (x <= r) {
-            if(Math.sqrt(Math.pow(x,2)+Math.pow(r-y,2)) < r){
-                sum_y += r - y + 1;
-                x++;
-            }
-            else if(Math.sqrt(Math.pow(x,2)+Math.pow(r-y,2)) == r){
-                sum_y += r - y + 1;
-                line++;
-                System.out.println("line: "+x);
-                x++;
-            }
-            else y++;
+        long r2count = 0;
+        for (int i = 1; i <= r2; i++) {
+            int y2 = (int) Math.floor(Math.sqrt(((long) r2 * r2 - (long) i * i))) + 1;
+            r2count += y2;
         }
+        r2count = r2count * 4 + 1;
 
-        if (flag) return line * 4;
-        else return sum_y * 4;
+        long r1count = 0;
+        long edge = 0;
+        for (int i = 1; i <= r1; i++) {
+            double yPowSqrt = Math.sqrt(((long) r1 * r1 - (long) i * i));
+            int y1 = (int) Math.floor(yPowSqrt) + 1;
+            r1count += y1;
+            if (yPowSqrt % 1 == 0.0)
+                edge += 1;
+        }
+        r1count = r1count * 4 + 1;
+        return r2count - r1count + edge * 4;
     }
 }
