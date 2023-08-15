cbd8d3851e02fa6f1224bdae42020a694aee6233
안원석 <95980876+Ahnwonseok@users.noreply.github.com>
2023-08-15 23:34:09 +0900

    [unrated] Title: 요격 시스템, Time: 0.51 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -4,7 +4,7 @@ class Solution {
     public static int solution(int[][] targets) {
         int answer = 0;   
         Arrays.sort(targets, (o1, o2) -> { return o1[1]-o2[1]; });
-   
+     
         int last = 0; 
         
         for(int[] target : targets){
